7bd5c0c4dde767451a255a27fe67002c80bbb562
utsahice <utsahic.encircle@gmail.com>
2025-10-13 18:45:03 +0530

    add task1

---
+++ task1/src/App.jsx
@@ -0,0 +1,12 @@
+
+import Portfolio from './portfolio'
+
+function App() {
+const bio = "I’m a Full Stack Developer And Working On React Fundamentals";
+
+  return (
+    <Portfolio bio={bio}/>
+  );
+}
+
+export default App
---
+++ task1/src/portfolio.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import profilePic from '/assets/u1.jpg'; 
+
+function Portfolio({ bio }) {
+  return (
+    <div style={styles.container}>
+      <img src={profilePic} alt="Profile" style={styles.image} />
+      <h1 style={styles.name}>Utsahi Chauhan</h1>
+      <p style={styles.bio}>{bio}</p>
+    </div>
+  );
+}
+
+const styles = {
+  
+  image: {
+    width: '150px',
+    height: '150px',
+    borderRadius: '50%',
+    objectFit: 'cover',
+  },
+  name: {
+    margin: '16px 0 8px',
+  },
+  bio: {
+    fontSize: '16px',
+    color: '#5e5e5e',
+  },
+};
+
+export default Portfolio;
